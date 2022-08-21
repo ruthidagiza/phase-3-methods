80d16345d58d7c468d0a5e458d5cd570cd0ff265
IDAGIZA <ruthidagiza@gmail.com>
2022-08-21 14:52:36 +0300

    commit tree a9259e01845af22ecbb43fa905efd127d50d5dfe

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet ("Naureen");
+
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+greet_with_default("Sunny");
+
+
+
+def add(num1, num2)
+    num1 + num2
+end
+add(1,3)
+
+
+def halve(number)
+    if number.class !=Integer
+        return nil
+    end
+    number / 2
+end
+halve(4)
